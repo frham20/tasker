2a994a8c0a05cc9b74053af099ed3cb0aa614f2c
Francois Hamel <francois.hamel@gmail.com>
2018-01-06 00:22:48 -0500

    Better logging to temp file as well as stdout

--- tasker/__main__.py
+++ tasker/__main__.py
@@ -2,26 +2,43 @@
 Main
 """
 import sys
+import os
 import logging
 import argparse
+import tempfile
 from tasker.config import load_config, run_config
 
 def main():
     """Package ran as exec"""
 
+    # Parse command line args
     parser = argparse.ArgumentParser(description='Executes a serie of tasks')
     parser.add_argument('filename', type=str,
                         help='The filename of the JSON file containing the tasks to execute')
     args = parser.parse_args()
 
-    logging.basicConfig(
-        stream=sys.stdout,
-        level=logging.INFO,
-        format='%(asctime)s:%(levelname)s:%(message)s'
-    )
+    # Initialize logger
+    log_level = logging.INFO
+    if __debug__:
+        log_level = logging.DEBUG
 
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger()
+    logger.setLevel(log_level)
 
+    log_formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+
+    log_sh = logging.StreamHandler(sys.stdout)
+    log_sh.setLevel(log_level)
+    log_sh.setFormatter(log_formatter)
+    logger.addHandler(log_sh)
+
+    log_fd, log_filename = tempfile.mkstemp(suffix='.log', prefix='tasker_', dir=None, text=True)
+    log_fh = logging.FileHandler(log_filename)
+    log_fh.setLevel(log_level)
+    log_fh.setFormatter(log_formatter)
+    logger.addHandler(log_fh)
+
+    # Load config and run the tasks
     try:
         logger.info("Loading config file...")
         cfg = load_config(args.filename)
@@ -33,6 +50,12 @@ def main():
     except Exception as ex:
         logger.exception(ex)
 
+    # Close log file
+    os.close(log_fd)
+
+    # If there were some errors, open the log
+    # TODO, parse log for :ERROR: or :CRITICAL: tags
+    os.startfile(log_filename)
 
 assert __name__ == '__main__'
 main()
